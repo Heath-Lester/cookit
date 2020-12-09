d97bbe38b70624563f5cb3cff4c72cd4cf6008df
Heath Lester <h.allen.lester@gmail.com>
2020-12-09 16:30:36 -0600

    able to render search results from api

--- src/components/search/SearchProvider.js
+++ src/components/search/SearchProvider.js
@@ -1,5 +1,6 @@
 
 import React, { useState } from "react"
+import { apiKey } from "../.api_key.js"
 
 
 export const SearchContext = React.createContext()
@@ -15,20 +16,17 @@ export const SearchProvider = props => {
 
 
     const searchRecipeByKeyword = keyword => {
-        return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${keyword}&number=100&instructionsRequired=true`, {
+        return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${keyword}&number=10&instructionsRequired=true`, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "2cc3b10740msh0b17a753309ab3dp125266jsnec84fbbfd9bc",
+                "x-rapidapi-key": `${apiKey}`,
                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
             }
         })
-            .then(response => {
-                console.log(response);
-            })
-            .catch(err => {
-                console.error(err);
-            })
-            .then(setResults);
+            .then(response => response.json())
+            .then(response => setResults(response.results))
+            .then(console.log(searchResults))
+            .catch(err => console.error(err))
     }
 
 
@@ -36,7 +34,7 @@ export const SearchProvider = props => {
         return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${searchObj.keyword}&diet=${searchObj.diet}&excludeIngredients=${searchObj.excludedIngredients}&intolerances=${searchObj.intolerances}&number=${searchObj.numberOfResults}&offset=${searchObj.SkipResults}&type=${searchObj.TypeOfCourse}&instructionsRequired=${searchObj.includeInstruction}`, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "2cc3b10740msh0b17a753309ab3dp125266jsnec84fbbfd9bc",
+                "x-rapidapi-key": `${apiKey}`,
                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
             }
         })
@@ -54,7 +52,7 @@ export const SearchProvider = props => {
         return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${ingredient}&number=100&ranking=1&ignorePantry=false`, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "2cc3b10740msh0b17a753309ab3dp125266jsnec84fbbfd9bc",
+                "x-rapidapi-key": `${apiKey}`,
                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
             }
         })
@@ -70,10 +68,10 @@ export const SearchProvider = props => {
 
     const recipeAutocomplete = RecipeTerm => {
         // debugger
-        return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/autocomplete?query=${RecipeTerm}&number=25`, {
+        return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/autocomplete?query=${RecipeTerm}&number=5`, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "2cc3b10740msh0b17a753309ab3dp125266jsnec84fbbfd9bc",
+                "x-rapidapi-key": `${apiKey}`,
                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
             }
         })
@@ -88,7 +86,7 @@ export const SearchProvider = props => {
         return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?query=${IngredientTerm}&number=25`, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "2cc3b10740msh0b17a753309ab3dp125266jsnec84fbbfd9bc",
+                "x-rapidapi-key": `${apiKey}`,
                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
             }
         })
