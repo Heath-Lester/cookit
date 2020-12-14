7d714b3448a67ee6b47b523175c1ec54235cd5e4
Heath Lester <h.allen.lester@gmail.com>
2020-12-14 11:23:38 -0600

    added viewport context with state

---
+++ src/components/viewport/ViewPortContext.js
@@ -0,0 +1,21 @@
+
+// import React, { useState } from "react"
+// import "./ViewPort.css"
+
+// export const ViewPortContext = React.createContext()
+
+// export const ViewPortDisplay = props => {
+
+//     const [viewPort, setViewPort] = useState()
+
+//     console.log("View Port Context", viewPort)
+
+
+//     return (
+//         <ViewPortContext.Provider value={{
+//             viewPort, setViewPort
+//         }}>
+//             {props.children}
+//         </ViewPortContext.Provider>
+//     )
+// }
