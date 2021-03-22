49b7615a311e6e9209c379f73df63abdf233b369
Heath Lester <h.allen.lester@gmail.com>
2021-03-22 14:37:39 -0500

    updated meal providers

--- src/components/meal/MealProvider.js
+++ src/components/meal/MealProvider.js
@@ -1,6 +1,5 @@
 
 import React, { useState } from "react"
-import { apiKey } from "../../.api_key.js"
 
 
 export const MealContext = React.createContext()
@@ -10,56 +9,55 @@ export const MealProvider = props => {
 
     const [meals, setMeals] = useState([])
 
-    const userId = parseInt(localStorage.getItem("cookit_user"))
+    const userToken = localStorage.getItem("cookit_user")
 
 
     const getMeals = () => {
-        // debugger
-        return fetch(`http://localhost:8088/mealsToPrep/?userId=${userId}`)
+        return fetch(`http://localhost:8088/meals`, {
+            headers: { "Authorization": `Token ${userToken}` }
+        })
+            .then(result => result.json())
+            .then(setMeals)
+    }
+
+    const addMeal = mealObj => {
+        return fetch(`http://localhost:8088/meals`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Token ${userToken}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(mealObj)
+        })
+            .then(getMeals)
+    }
+
+    const deleteMeal = mealId => {
+        return fetch(`http://localhost:8088/meals/${mealId}`, {
+            method: "DELETE",
+            headers: { "Authorization": `Token ${userToken}` }
+        })
+            .then(getMeals)
+    }
+
+    const resetMeals = () => {
+        return fetch(`http://localhost:8088/meals/complete`, {
+            method: "DELETE",
+            headers: { "Authorization": `Token ${userToken}` }
+        })
             .then(result => result.json())
             .then(setMeals)
     }
 
 
-    const addMeal = recipeObj => {
-        // debugger
-        return fetch(`http://localhost:8088/mealsToPrep`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(recipeObj)
-        })
-            .then(getMeals)
-    }
-
-
-    const deleteMeal = mealId => {
-        return fetch(`http://localhost:8088/mealsToPrep/${mealId}`, {
-            method: "DELETE",
-        })
-            .then(getMeals)
-    }
-
-
-    const getRecipe = id => {
-        return fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`, {
-            method: "GET",
-            headers: {
-                "x-rapidapi-key": `${apiKey}`,
-                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-            }
-        })
-            .then(response => response.json())
-    }
-
-    
     return (
         <MealContext.Provider value={{
             meals,
             setMeals,
             getMeals,
+            addMeal,
             deleteMeal,
-            getRecipe,
-            addMeal
+            resetMeals
         }}>
             {props.children}
         </MealContext.Provider>
