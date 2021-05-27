9a210c23a4245730bcfe9f39fa1b44288917c18c
Heath Lester <h.allen.lester@gmail.com>
2021-05-27 11:21:16 -0500

    added the damn spinner

--- src/components/viewport/SearchResults.js
+++ src/components/viewport/SearchResults.js
@@ -3,6 +3,7 @@ import React, { useContext } from "react"
 import { ViewPortContext } from "./ViewPortContext"
 import { SearchContext } from "../search/SearchProvider"
 import { ViewPort } from "./ViewPort"
+import Spinner from 'react-bootstrap/Spinner'
 import "./ViewPort.css"
 
 
@@ -15,26 +16,30 @@ export const SearchResults = (props) => {
     const imageUrl = `https://spoonacular.com/recipeImages/`
 
 
-    return (
-        <section className="search__results">
-            {
-                searchResults.map(result => {
-                    return <div className="results__recipe" id={"recipe" + result.id} autoFocus key={"recipe" + result.id}
-                        onClick={() => {
-                            getRecipeById(result.id)
-                            setViewPort(2)
-                            return <ViewPort {...props} />
-                        }}>
-                        <img className="results__image" src={imageUrl + result.image} alt={`Recipe`} />
-                        <div className="results__details">
-                            <h3 className="results__name">{result.title}</h3>
-                            <dt>Ready in {result.readyInMinutes} minutes</dt>
-                            <dt>Serves {result.servings}</dt>
-                            <dt><a href={result.sourceUrl}>Original Recipe</a></dt>
-                        </div>
-                    </div>
-                })
-            }
-        </section>
-    )
+    // if (searchResults.length === 0) {
+        return <Spinner className="search__spinner" animation="border" size="lg" role="status" variant="warning" />
+    // } else if (searchResults.length > 0) {
+    //     return (
+    //         <section className="search__results">
+    //             {
+    //                 searchResults.map(result => {
+    //                     return <div className="results__recipe" id={"recipe" + result.id} autoFocus key={"recipe" + result.id}
+    //                         onClick={() => {
+    //                             getRecipeById(result.id)
+    //                             setViewPort(2)
+    //                             return <ViewPort {...props} />
+    //                         }}>
+    //                         <img className="results__image" src={imageUrl + result.image} alt={`Recipe`} />
+    //                         <div className="results__details">
+    //                             <h3 className="results__name">{result.title}</h3>
+    //                             <dt>Ready in {result.readyInMinutes} minutes</dt>
+    //                             <dt>Serves {result.servings}</dt>
+    //                             <dt><a href={result.sourceUrl}>Original Recipe</a></dt>
+    //                         </div>
+    //                     </div>
+    //                 })
+    //             }
+    //         </section>
+    //     )
+    // }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
